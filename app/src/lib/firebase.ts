6cd8f0a0fee2b253739d0d1d52b748058fa808a6
achong <alistair.chong@gmail.com>
2026-05-27 03:16:09 +0000

    Fix Firebase API key configuration

--- app/src/lib/firebase.ts
+++ app/src/lib/firebase.ts
@@ -2,11 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAROc3TUVMLIbvfgOzsKkJKGdGDYOC2sc",
+  apiKey: "AIzaSyAROc3TUVMLIbvfg0zsKkJKGdGDGYOC2sc",
   authDomain: "tafetas-3ac45.firebaseapp.com",
   projectId: "tafetas-3ac45",
-  storageBucket: "tafetas-3ac45.appspot.com",
+  storageBucket: "tafetas-3ac45.firebasestorage.app",
   messagingSenderId: "594676311320",
   appId: "1:594676311320:web:990ed781daf128ece8aa6d",
   measurementId: "G-ZVQ1XXG169"
@@ -16,3 +17,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
