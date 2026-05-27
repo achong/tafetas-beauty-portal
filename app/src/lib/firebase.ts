ba0fb50613b4efe67794544d2a1eead8282800e0
achong <alistair.chong@gmail.com>
2026-05-27 00:14:43 +0000

    Update Firebase config with correct API Key

--- app/src/lib/firebase.ts
+++ app/src/lib/firebase.ts
@@ -4,10 +4,10 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAROc3TUVMLIbvfgOzsKkJKGdGDYOC2sc",
+  apiKey: "AIzaSyAROc3TUVMLIbvfg0zsKkJKGdGDGYOC2sc",
   authDomain: "tafetas-3ac45.firebaseapp.com",
   projectId: "tafetas-3ac45",
-  storageBucket: "tafetas-3ac45.appspot.com",
+  storageBucket: "tafetas-3ac45.firebasestorage.app",
   messagingSenderId: "594676311320",
   appId: "1:594676311320:web:990ed781daf128ece8aa6d",
   measurementId: "G-ZVQ1XXG169"
